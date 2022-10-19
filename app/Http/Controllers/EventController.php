1899a2a3d4980e951e9c4e18af6167720a83108f
Crislaine Luana <clpaula3007@gmail.com>
2022-10-19 17:29:17 -0300

    Inserindo operação de busca, upload de imagens

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -10,9 +10,18 @@ class EventController extends Controller
     
     public function index(){
         
-        $events = Event::all();
+        $search = request('search');
+        if($search){
+            $events = Event::where([
+                ['title', 'like', '%'.$search.'%']
+            ])->get();
+        }else{
+            $events = Event::all();
+        }
 
-        return view('welcome', ['events' => $events]);
+
+       
+        return view('welcome', ['events' => $events, 'search' => $search]);
 
     }
 
@@ -25,13 +34,35 @@ class EventController extends Controller
         $event = new Event;
 
         $event->title = $request->title;
+        $event->date = $request->date;
         $event->description = $request->description;
         $event->city = $request->city;
         $event->private = $request->private;
+        $event->items = $request->items;
+
+        //image Upload
+        if($request->hasFile('image') && $request->file('image')->isValid()){
+
+            $requestImage = $request->image;
+            $extension = $requestImage->extension();
+            $imageName = md5($requestImage->getClientOriginalName().strtotime("now")) . ".".$extension;
+
+            $requestImage->move(public_path('img/events'), $imageName);
+
+            $event->image = $imageName;
+        }
 
         $event->save();
 
-        return redirect('/');
+        return redirect('/')->with('msg', 'Evento criado com sucesso!');
+    }
+
+    public function show($id){
+
+        $event = Event::findorFail($id);
+
+        return view('events.show', ['event' => $event]);
+
     }
 
 }
--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -4,13 +4,21 @@
 
 @section('content')
 
-    <div id="event-criate-container" class="col-md-6 offset-md-3">
+    <div id="event-create-container" class="col-md-6 offset-md-3">
         <h1>Crie o seu evento</h1>
-        <form action="/events" method="POST">
+        <form action="/events" method="POST" enctype="multipart/form-data">
             @csrf
+            <div class="form-group">
+                <label for="image">Imagem do evento:</label>
+                <input type="file" class="form-control-file" id="image" name="image" placeholder="Imagem do Evento">
+            </div>
             <div class="form-group">
                 <label for="title">Evento:</label>
                 <input type="text" class="form-control" id="title" name="title" placeholder="Nome do Evento">
+            </div>
+            <div class="form-group">
+                <label for="date">Data do Evento:</label>
+                <input type="date" class="form-control" id="date" name="date" placeholder="Nome do Evento">
             </div>
             <div class="form-group">
                 <label for="city">Cidade:</label>
@@ -27,6 +35,27 @@
                 <label for="description">Descrição evento:</label>
                 <textarea name="description" id="description" class="form-control" placeholder="O que acontecerá no evento?"></textarea>
             </div>
+
+            <div class="form-group">
+                <label for="description">Adicione Itens de infraestrutura:</label>
+                <div class="form-group">
+                    <input type="checkbox" name="items[]" value="Cadeiras"> Cadeiras
+                </div>
+                <div class="form-group">
+                    <input type="checkbox" name="items[]" value="Palco"> Palco
+                </div>
+                <div class="form-group">
+                    <input type="checkbox" name="items[]" value="Cerveja grátis"> Cerveja grátis
+                </div>
+                <div class="form-group">
+                    <input type="checkbox" name="items[]" value="Open Food"> Open Food
+                </div>
+                <div class="form-group">
+                    <input type="checkbox" name="items[]" value="Brindes"> Brindes
+                </div>
+            </div>
+
+
             <button type="submit" class="btn btn-primary">Criar um evento</button>
         </form>
 
