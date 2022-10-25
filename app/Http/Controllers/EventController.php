7713187ad4487c3148cc5c1045f6913cbf378367
Crislaine Luana <clpaula3007@gmail.com>
2022-10-25 17:54:14 -0300

    Resolução de problemas de configurção

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Event;
+use App\Models\User;
 
 class EventController extends Controller
 {
@@ -52,6 +53,9 @@ class EventController extends Controller
             $event->image = $imageName;
         }
 
+        $user = auth()->user();
+        $event->user_id = $user->id;
+
         $event->save();
 
         return redirect('/')->with('msg', 'Evento criado com sucesso!');
@@ -61,8 +65,19 @@ class EventController extends Controller
 
         $event = Event::findorFail($id);
 
-        return view('events.show', ['event' => $event]);
+        $eventOwner = User::where('id', $event->user_id)->first()->toArray();
 
+        return view('events.show', ['event' => $event,'eventOwner' => $eventOwner]);
+
+    }
+
+
+    public function dashboard(){
+        $user = auth()->user();
+
+        $events = $user->events;
+
+        return view('events.dashboard', ['events' => $events]);
     }
 
 }
--- app/Models/Event.php
+++ app/Models/Event.php
@@ -15,4 +15,8 @@ class Event extends Model
 
 
     protected $dates = ['dates'];
+
+    public function user(){
+        return $this->belongsTo('App\Models\User');
+    }
 }
